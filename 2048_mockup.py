5b89570896a6d2c23a1614c86d76f9c6758c5206
Heejong Ahn <heejongahn@gmail.com>
2015-01-18 15:53:38 +0900

    Added a main routine

--- 2048_mockup.py
+++ 2048_mockup.py
@@ -4,6 +4,7 @@ print '=============================================='
 print 'Simple mockup of 2048 game, made by Heejong Ahn'
 print 'You can see the code in github.com/heejongahn'
 print '=============================================='
+print '\n'
 
 n = input('Size of the table? ')
 
@@ -19,6 +20,36 @@ def printTable(table):
         for e in line:
             print e,
         print '\n'
+    return
+
+def pickGrid(table, freeList):
+    grid = random.choice(freeList)
+    table[grid//4][grid%4] = 2
+    return
+
+def makeMove(usrInput):
+    if usrInput == 'up':
+        moveUp
+    elif usrInput == 'down':
+        moveDown
+    elif usrInput == 'left':
+        moveLeft
+    elif usrInput == 'right':
+        moveRight
+    else:
+        print 'Please select a valid move'
+        return 1
+    return 0
+
 
 table = makeTable(n)
 printTable(table)
+
+
+while (freeList!= []):
+    while True:
+        usrInput = input("Pick a move : up/down/left/right ")
+        if not makeMove(usrInput):
+            break
+
+    pickGrid
