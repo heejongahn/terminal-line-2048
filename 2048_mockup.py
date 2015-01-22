9f0954bd5e8ad67eb8e198d8dd182aed4be8bc0d
Heejong Ahn <heejongahn@gmail.com>
2015-01-23 00:04:49 +0900

    OOP-ized

--- 2048_mockup.py
+++ 2048_mockup.py
@@ -3,101 +3,109 @@ import random
 # 2048 Mockup in Python3 : Made by Heejong Ahn
 
 
-# function initializeGame
-# Literally initializes the game: Make a game table and print it
+class Table():
 
-def initializeGame():
-    global table
-    global n
 
-    print ('==============================================')
-    print ('Simple mockup of 2048 game, made by Heejong Ahn')
-    print ('You can see the full code in github.com/heejongahn')
-    print ('==============================================')
-    print ('\n')
+    # function __init__
+    # Initializes the game: Make a game table and print it
 
-    n = int(input('Size of the table? '))
-    table = makeTable(n)
+    def __init__(self):
 
-    printTable(table)
-
-# function makeTable
-# Makes a n*n size game table
-
-def makeTable(n):
-    table = [0] * n
-    for i in range(n):
-        table[i] = ['-'] * n
-    table[0][0] = 2
-    return table
-
-# function printTable
-# Prints current game table state
-
-def printTable(table):
-    for line in table:
-        for e in line:
-            print (e),
+        print ('==============================================')
+        print ('Simple mockup of 2048 game, made by Heejong Ahn')
+        print ('You can see the full code in github.com/heejongahn')
+        print ('==============================================')
         print ('\n')
-    return
 
-# function pickGrid
-# Pick a free grid from a table and place [2] tile
+        self.size = int(input('Size of the table? '))
+        self.table = self.makeTable()
+        self.freeList = list(range(1,16))
 
-def pickGrid(table, freeList):
-    global n
+        self.printTable()
 
-    grid = random.choice(freeList)
-    table[grid//n][grid%n] = 2
-    return
+    # function makeTable
+    # Makes a size*size size game table and return it
 
-# function makeMove
-# This is what 2048 is about!
+    def makeTable(self):
+        size = self.size
 
-def makeMove(usrInput):
-    if usrInput == 'up':
-        moveUp()
-    elif usrInput == 'down':
-        moveDown()
-    elif usrInput == 'left':
-        moveLeft()
-    elif usrInput == 'right':
-        moveRight()
-    else:
-        print ('Please select a valid move')
-        return 1
-    return 0
+        table = [0] * size
+        for i in range(size):
+            table[i] = ['-'] * size
+        table[0][0] = 2
+        return table
 
-# funciton moveUp
-# Like other three move functions below,
-# 1) Handles the merge first and then
-# 2) Handles the blanks
+    # function printTable
+    # Prints current game table state
 
-def moveUp():
-    global table
-    global n
+    def printTable(self):
+        table = self.table
 
-    for col in range(0, n):
-        for i in range(0, n-1):
-            for j in range(i+1, n):
-                if table[i][col]!= '-' and table[i][col] == table[j][col]:
-                    table[j][col] = '-'
-                    table[i][col] = table[i][col] * 2
-                    break
+        for line in table:
+            for e in line:
+                print (e, end=" ")
+            print ('\n')
+        return
 
-        for i in range(0, n-1):
-            for j in range(i+1, n):
-                if table[i][col]== '-' and table[j][col]!= '-':
-                    table[i][col] = table[j][col]
-                    table[j][col] = '-'
-                    break
+    # function pickGrid
+    # Pick a free grid from a table and place [2] tile
+
+    def pickGrid(self):
+        size = self.size
+
+        grid = random.choice(self.freeList)
+        self.freeList.remove(grid)
+        self.table[grid//size][grid%size] = 2
+
+    # function makeMove
+    # This is what 2048 is about!
+
+    def makeMove(self, usrInput):
+        if usrInput == 'w':
+            self.moveUp()
+        elif usrInput == 's':
+            self.moveDown()
+        elif usrInput == 'a':
+            self.moveLeft()
+        elif usrInput == 'd':
+            self.moveRight()
+        else:
+            print ('Please select a valid move')
+            return 1
+        return 0
+
+    # funciton moveUp
+    # Like other three move functions below,
+    # 1) Handles the merge first and then
+    # 2) Handles the blanks
+
+    def moveUp(self):
+        n = self.size
+        table = self.table
+
+        for col in range(0, n):
+            for i in range(0, n-1):
+                for j in range(i+1, n):
+                    if table[i][col]!= '-' and table[i][col] == table[j][col]:
+                        table[j][col] = '-'
+                        table[i][col] = table[i][col] * 2
+                        break
+
+            for i in range(0, n-1):
+                for j in range(i+1, n):
+                    if table[i][col]== '-' and table[j][col]!= '-':
+                        table[i][col] = table[j][col]
+                        table[j][col] = '-'
+                        break
 
 
 # Main routine
 
-while (freeList!= []):
-    usrInput = input("Pick a move : up/down/left/right ")
-    if makeMove(usrInput):
+table = Table()
+
+while (table.freeList!= []):
+    usrInput = input("Pick a move : w(up) / s(down) / a(left) / d(right) ")
+    if table.makeMove(usrInput):
         continue
-    pickGrid()
-    printTable()
+    table.pickGrid()
+    table.printTable()
